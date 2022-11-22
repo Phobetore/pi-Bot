5200621e5b0fef5783996ae949f875b630c6bd58
Phobetore <broucewayn1413@protonmail.com>
2022-11-22 13:45:20 +0100

    commands loader

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 
-const Discord =  require("discord.js")
-const intents = new Discord.IntentsBitField(8)
-const client = new Discord.Client({intents})
-const config = require("./config.json")
-//const loadCommand = require("./Loader/loadCommand.js")
+import { IntentsBitField, Client, Events, ActivityType } from "discord.js";
+const intents = new IntentsBitField(8)
+const client = new Client({intents})
+import { token } from "./config.json";
+const fs = require('node:fs');
+const path = require('node:path');
+
+
+
+// commands loading
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+
 
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Discord.Events.ClientReady, c => {
+client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(config.token);
 
+
+
+// Log in to Discord with your client's token
+client.login(token);
+
+
+
+
+// setting an activity
 client.on('ready', () => {
 	
     const activities = [
@@ -27,7 +56,7 @@ client.on('ready', () => {
 
     setInterval(()=>{
     const inter = activities[Math.floor(Math.random()*activities.length)];
-    client.user.setActivity(inter, { type: Discord.ActivityType.Watching})},8000
+    client.user.setActivity(inter, { type: ActivityType.Watching})},8000
     );
 
 });
