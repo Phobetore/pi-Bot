5f9a7e758d3ae94b361f935d9446cdd994b23bc9
Phobetore <broucewayn1413@protonmail.com>
2022-11-22 16:16:05 +0100

    deploy-commands script

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const path = require('node:path');
 
 
 
+
 // commands loading
 client.commands = new Discord.Collection();
 
@@ -24,6 +25,24 @@ for (const file of commandFiles) {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
+client.on(Discord.Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 
 
