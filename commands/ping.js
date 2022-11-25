82a0d04a820960a6aa96ee8bb58547e5620eac4c
Phobetore <broucewayn1413@protonmail.com>
2022-11-25 23:22:59 +0100

    ms bug fix

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,6 @@ module.exports = {
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		
-		await interaction.reply('Pong! `'+( interaction.createdTimestamp - Date.now())+'ms`');
+		await interaction.reply('Pong! `'+(Date.now() - interaction.createdTimestamp)+'ms`');
 	},
 };
